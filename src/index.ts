63f25c0b60aa54f71d46ed8eae8b056c8a1c8a23
Arti <59352535+artifishvr@users.noreply.github.com>
2025-02-24 21:45:27 -0800

    fix examples still being here

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
 import { inspect } from "./inspect";
 import { info, warn, error, success } from "./prettyprint";
 
-inspect([1, 2, 3]);
-inspect({ a: 1, b: 2, c: 3 });
-inspect("Hello, world!");
-inspect(42);
-inspect(null);
-inspect(true);
+// inspect([1, 2, 3]);
+// inspect({ a: 1, b: 2, c: 3 });
+// inspect("Hello, world!");
+// inspect(42);
+// inspect(null);
+// inspect(true);
 
-warn("Watch out, world!");
-error("Help, world!");
-success("Hooray, world!");
-info("Hello, world!");
+// warn("Watch out, world!");
+// error("Help, world!");
+// success("Hooray, world!");
+// info("Hello, world!");
 
-info("Hello, world!", "Label");
-info("Hello, world!", { label: "Label", timestamp: false });
-info("Hello, world!", false);
+// info("Hello, world!", "Label");
+// info("Hello, world!", { label: "Label", timestamp: false });
+// info("Hello, world!", false);
 
 export { inspect, info, warn, error, success };
